ddb187990046936b6ee11ad3aa538fed7032f6aa
Charan-47 <62457412+Charan-47@users.noreply.github.com>
2020-10-15 12:21:01 +0530

    Add files via upload

---
+++ Assignment-2/src/com/Assignment2/Main.java
@@ -0,0 +1,102 @@
+package com.Assignment2;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter subject1 marks : ");
+        int subject1 = sc.nextInt();
+        System.out.println("Enter subject2 marks : ");
+        int subject2 = sc.nextInt();
+        System.out.println("Enter subject3 marks : ");
+        int subject3 = sc.nextInt();
+        System.out.println("Enter subject4 marks : ");
+        int subject4 = sc.nextInt();
+        System.out.println("Enter subject5 marks : ");
+        int subject5 = sc.nextInt();
+
+        int add = subject1+subject2+subject3+subject4+subject5;
+        int percentage = (add * 100) / 500;
+        //System.out.println(add);
+        //System.out.println(percentage);
+
+        if (percentage >= 90 && percentage <= 100)
+        {
+            System.out.println("The student scored "+"O grade "+"and the percentage obtained is "+percentage);
+        }
+        else if (percentage >= 80 && percentage < 90)
+        {
+            System.out.println("The student scored "+"A+ grade "+"and the percentage obtained is "+percentage);
+        }
+        else if (percentage >= 70 && percentage < 80)
+        {
+            System.out.println("The student scored "+"A grade "+"and the percentage obtained is "+percentage);
+        }
+        else if (percentage >= 60 && percentage < 70)
+        {
+            System.out.println("The student scored "+"B+ grade "+"and the percentage obtained is "+percentage);
+        }
+        else if (percentage >= 50 && percentage < 60)
+        {
+            System.out.println("The student scored "+"B grade "+"and the percentage obtained is "+percentage);
+        }
+        else
+        {
+            System.out.println("Failed");
+        }
+
+        */
+
+        /*
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Employee name : ");
+        String name = sc.next() ;
+        System.out.println("Enter the date of birth : ");
+        int date = sc.nextInt();
+        System.out.println("Enter the month of birth : ");
+        int month = sc.nextInt();
+        System.out.println("Enter the year of birth : ");
+        int year = sc.nextInt();
+        System.out.println("Enter the monthly salary : ");
+        int monthly_salary = sc.nextInt();
+
+        int annual_salary;
+        annual_salary = monthly_salary * 12;
+
+        int age;
+        age = 2020 - year;
+
+
+
+        if (annual_salary >= 500000)
+        {
+            System.out.println("The name is "+name+", the age of employee is "+age+", the annual salary of the employee is "+annual_salary+" and the tax amount is "+ annual_salary * 0.2);
+        }
+        else if (annual_salary >= 400000)
+        {
+            System.out.println("The name is "+name+", the age of employee is "+age+", the annual salary of the employee is "+annual_salary+" and the tax amount is "+ annual_salary * 0.15);
+        }
+        else if (annual_salary >= 300000)
+        {
+            System.out.println("The name is "+name+", the age of employee is "+age+", the annual salary of the employee is "+annual_salary+" and the tax amount is "+ annual_salary * 0.1);
+        }
+        else if (annual_salary >= 200000)
+        {
+            System.out.println("The name is "+name+", the age of employee is "+age+", the annual salary of the employee is "+annual_salary+" and the tax amount is "+ annual_salary * 0.05);
+        }
+        else
+        {
+            System.out.println("The name is "+name+", the age of employee is "+age+", the annual salary of the employee is "+annual_salary+" and the tax amount is "+"zero");
+        }
+
+        */
+
+    }
+}
