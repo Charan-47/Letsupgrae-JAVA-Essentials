c97e07164482b78f3d0d43276e32bcd9b6d58f6c
Charan-47 <62457412+Charan-47@users.noreply.github.com>
2020-10-15 13:39:36 +0530

    Add files via upload

---
+++ Assignment-3/src/com/Assignment3/Avenger.java
@@ -0,0 +1,38 @@
+package com.Assignment3;
+
+import java.util.Scanner;
+
+public class Avenger {
+/*
+    Scanner sc = new Scanner(System.in);
+
+    String name;
+    int age;
+    String power;
+    String weapon;
+    String planet;
+
+    public void getDetails()
+    {
+
+        System.out.println("Enter the name : ");
+        name = sc.next();
+        System.out.println("Enter the age : ");
+        age = sc.nextInt();
+        System.out.println("Enter the power : ");
+        power = sc.next();
+        System.out.println("Enter the weapon : ");
+        weapon = sc.next();
+        System.out.println("Enter the planet : ");
+        planet = sc.next();
+
+    }
+
+    public void displayDetails()
+    {
+        System.out.println("The name of the Avenger is "+ name +" ,the age of the Avenger is "+ age +" , avenger has the power of "+ power +" avenger has a weapon called "+ weapon +" this avenger is from the planet "+ planet);
+    }
+
+ */
+}
+
---
+++ Assignment-3/src/com/Assignment3/Main.java
@@ -0,0 +1,79 @@
+package com.Assignment3;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+        /*              // question-1
+
+        //array creation
+        Avenger[] avengers = new Avenger[5];
+
+        //object creation and getDetails
+        for (int i = 0;i < 5; i++)
+        {
+            avengers[i] = new Avenger();
+        }
+        for (int i = 0;i < 5; i++)
+        {
+            avengers[i].getDetails();
+        }
+
+        //displayDetials
+        for (int i = 0;i < 5; i++)
+        {
+            avengers[i].displayDetails();
+        }
+
+        */
+
+
+        /*                    Question-2
+
+        int[] arr = new int[5];
+
+        int i;
+
+        System.out.println("Enter the number : ");
+        for ( i = 0;i < 4; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        for ( i = 0;i < 4; i++)
+        {
+            if((arr[i] % 2) !=0)
+            {
+                System.out.println(arr[i]);
+            }
+        }
+
+         */
+
+
+        /*               question-3
+
+        int[] arr = new int[5];
+
+        int i;
+        int sum = 0;
+
+        System.out.println("Enter the number : ");
+        for ( i = 0;i < 4; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        for ( i = 0;i < 4; i++)
+        {
+           sum = sum +arr[i];
+        }
+        System.out.println(sum);
+
+        */
+
+    }
+}
