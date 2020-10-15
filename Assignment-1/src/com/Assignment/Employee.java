3548cb5298b12041597bf7975b82099601c7a482
Charan-47 <62457412+Charan-47@users.noreply.github.com>
2020-10-15 12:20:15 +0530

    Add files via upload

---
+++ Assignment-1/src/com/Assignment/Employee.java
@@ -0,0 +1,14 @@
+package com.Assignment;
+
+public class Employee {
+    String name = "charan";
+    int age = 20;
+    String city = "chennai";
+
+    public void display()
+    {
+        System.out.println("The name is : "+name);
+        System.out.println("The age is : "+age);
+        System.out.println("the city is: "+city);
+    }
+}
---
+++ Assignment-1/src/com/Assignment/Main.java
@@ -0,0 +1,12 @@
+package com.Assignment;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Employee one = new Employee();
+        Employee two = new Employee();
+        one.display();
+        two.display();
+    }
+}
